7319853c41ef4adcbcabf1453dfe981a87aa3c53
Max Koretskyi <m.koretskyi@gmail.com>
2025-07-29 17:43:50 +0200

    Update test_python_columns_script_measure1.py

--- kywy-spec-tests-final/test_python_columns_script_measure1.py
+++ kywy-spec-tests-final/test_python_columns_script_measure1.py
@@ -1,14 +1,14 @@
 import pandas as pd
 
 from kywy.client.kawa_decorators import kawa_tool
-
+from slack_sdk import WebClient
 
 @kawa_tool(inputs={'measure1': float}, outputs={'message': str})
 def execute_new_decorator(df: pd.DataFrame):
     print('run: simple_join_script')
     print('[1]: Data from KAWA')
     print(df)
-    df['message'] = str(df['measure1']) + '_some_text'
+    df['message'] = str(WebClient)
     print('[2]: Data after script operation')
     print(df)
     return df
